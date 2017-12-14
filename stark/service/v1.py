7fd8ae7c0066420d6815104d39154b875dd4c418
zbk name <649276525@qq.com>
2017-12-14 17:13:05 +0800

    动态生成url

--- stark/service/v1.py
+++ stark/service/v1.py
@@ -1,9 +1,34 @@
 from django.conf.urls import url
+from django.shortcuts import HttpResponse
 
 class  StarkConfig(object):
     def __init__(self,model_class,site):
         self.model_class = model_class
         self.site =site
+
+    def get_urls(self):
+        app_model_name = (self.model_class._meta.app_label,self.model_class._meta.model_name)
+        url_patterns = [
+            url(r'^$', self.changelist_view, name="%s/%s_changelist" % app_model_name),
+            url(r'^add/$', self.add_view, name="%s/%s_add" % app_model_name),
+            url(r'^(\d+)/delete/$', self.delete_view, name="%s/%s_delete" % app_model_name),
+            url(r'^(\d+)/change/$', self.change_view, name="%s/%s_change" % app_model_name),
+        ]
+        return url_patterns
+
+    def changelist_view(self,request,*args,**kwargs):
+        return HttpResponse('列表')
+    def add_view(self,request,*args,**kwargs):
+        return HttpResponse('添加')
+    def delete_view(self,request,*args,**kwargs):
+        return HttpResponse('删除')
+    def change_view(self,request,*args,**kwargs):
+        return HttpResponse('修改')
+
+    @property
+    def urls(self):
+        return self.get_urls()
+
 
 class StarkSite(object):
     '''
@@ -19,6 +44,15 @@ class StarkSite(object):
 
     def get_urls(self):
         url_pattern = []
+
+        for model_class,stark_config_obj in self._registry.items():
+            #为每一个类，创建4个url
+            app_name = model_class._meta.app_label #应用名
+            model_name = model_class._meta.model_name #类名
+
+            curd_url = url(r'^%s/%s/'%(app_name,model_name,),(stark_config_obj.urls,None,None))
+            url_pattern.append(curd_url)
+        return url_pattern
 
     @property
     def urls(self):
